07d599e7ded2f34db4c7d9b2d7567583e7cbb842
padfoot-12 <84430740+padfoot-12@users.noreply.github.com>
2021-05-20 10:27:00 +0530

    Add files via upload

---
+++ labex1.1py.py
@@ -0,0 +1,19 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a=int(input("Enter number "))
+Enter number 32
+>>> b=int(input("Enter number "))
+Enter number 72
+>>> c=int(input("Enter number "))
+Enter number 61
+>>> total=(a+b+c)
+>>> print(type(total))
+<class 'int'>
+>>> print(total)
+165
+>>> average=(a+b+c)/3
+>>> print(type(average))
+<class 'float'>
+>>> print(average)
+55.0
+>>> 
