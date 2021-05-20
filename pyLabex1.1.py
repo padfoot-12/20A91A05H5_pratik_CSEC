47bf315b425f3e2d4f5a1439d3516dbe8bf2b24a
padfoot-12 <84430740+padfoot-12@users.noreply.github.com>
2021-05-20 10:39:13 +0530

    Add files via upload

---
+++ pyLabex1.1.py
@@ -0,0 +1,24 @@
+>>> a=int(input("Enter number "))
+Enter number 32
+>>> b=int(input("Enter number "))
+Enter number 72
+>>> c=int(input("Enter number "))
+Enter number 61
+>>> total=(a+b+c)
+>>> print(type(total))
+<class 'int'>
+>>> print(total)
+165
+>>> average=(a+b+c)/3
+>>> print(type(average))
+<class 'float'>
+>>> print(average)
+55.0
+>>> #Output:
+>>> #Enter number 32
+>>> #Enter number 72
+>>> #Enter number 61
+>>> #<class 'int'>
+#165
+#<class 'float'>
+#55.0
