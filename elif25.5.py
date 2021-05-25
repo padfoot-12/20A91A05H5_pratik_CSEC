d55117caaf6cb7f626263aac0af19c45e34b1493
padfoot-12 <84430740+padfoot-12@users.noreply.github.com>
2021-05-25 14:27:44 +0530

    Add files via upload

---
+++ elif25.5.py
@@ -0,0 +1,11 @@
+a=int(input())
+if (a%2==0):
+    print('a is divisible by 2')
+elif (a%3==0):
+    print('a is divisible by 3')
+elif (a%5==0):
+    print('a is divisible by 5')
+else:
+    print('a is not divisible by 2,3 or 5')
+
+
