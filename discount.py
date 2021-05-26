63d805c7a7838847afe5dd3b256a6aeb8139e6e2
padfoot-12 <84430740+padfoot-12@users.noreply.github.com>
2021-05-26 14:19:47 +0530

    Add files via upload

---
+++ discount.py
@@ -0,0 +1,22 @@
+#program to calculate discount
+print('Enter price of purchases: ')
+x=int(input())
+if (x<1000):
+    print('no discount')
+    print('total bill amount',x,sep=' ')
+elif (1000<=x<=1999):
+    print('discount is 10%')
+    a=(x-(x*0.1))
+    print('total bill amount',a,sep=' ')
+elif (2000<=x<=2999):
+    print('discount is 20%')
+    b=(x-(x*0.2))
+    print('total bill amount',b,sep=' ')
+elif (3000<=x<=4999):
+    print('discount is 30%')
+    c=(x-(x*0.3))
+    print('total bill amount',c,sep=' ')
+else:
+    print('discount is 40%')
+    d=(x-(x*0.4))
+    print('total bill amount',d,sep=' ')
